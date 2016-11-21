be2e68b682784f66338d78b85165fc72c48765f9
Mohammad <moham1mad@gmail.com>
2016-11-21 15:00:29 +0100

    Initial commit

---
+++ app/src/main/java/dtu/grp20/puzzleflip/MainActivity.java
@@ -0,0 +1,13 @@
+package dtu.grp20.puzzleflip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
