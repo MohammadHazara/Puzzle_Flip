a7e2f2abb38d6c24f5c66f7a60a8221ae67d453d
Mohammad <moham1mad@gmail.com>
2016-11-21 16:35:20 +0100

    Commite 2

--- app/src/main/java/dtu/grp20/puzzleflip/MainActivity.java
+++ app/src/main/java/dtu/grp20/puzzleflip/MainActivity.java
@@ -2,13 +2,34 @@ package dtu.grp20.puzzleflip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startNewGame, continueGame;
+    ImageView imageView;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //du er en pleb
+        startNewGame = (Button) findViewById(R.id.startNewGameButton);
+        continueGame = (Button) findViewById(R.id.continueGameButton);
+        imageView = (ImageView) findViewById(R.id.imageViewMainActi);
+        textView = (TextView) findViewById(R.id.textView);
+
+
+
+
+
+
+
+
+
+
     }
 }
